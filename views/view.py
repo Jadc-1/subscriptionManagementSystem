9c98aeb39153bbd933d43f9ae5c8511872acb190
João Alexandre <joaoale.25@gmail.com>
2025-01-06 15:26:13 -0300

    Update view.py
    Added def create and list_all

--- views/view.py
+++ views/view.py
@@ -1,13 +1,27 @@
 import __init__
 from models.database import engine
 from models.model import Subscription
+from sqlmodel import Session, select
+from datetime import date
 
-class SubscriptionManagement:
+class SubscriptionManagement():
     def __init__(self, engine):
         self.engine = engine
 
-    def create(self, subscription: Subscription):
+    def create(self, subscription: Subscription): ## criamos a instância subscription que importa todos os dados do Subscription que criamos
+        with Session(self.engine) as session:
+            session.add(subscription)
+            session.commit()
+            return subscription
+        
+    def list_all(self):
+        with Session(self.engine) as session:
+            statement = select(Subscription) ##Busque os dados da tabela subscription (isso é uma query)
+            result = session.exec(statement) ##o exec é utilizado para executar querys, e estamos falando para executar statement, que vai selecionar a tabela inteira
+            return result
 
 sm = SubscriptionManagement(engine)
+subscription = Subscription(empresa='netflix', site='netflix.com.br', data_assinatura=date.today(), valor= '150')
+sm.create(subscription)
 
 
