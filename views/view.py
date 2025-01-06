8bd4169483cb0d16d64d92fe75692b092cc81565
João Alexandre <joaoale.25@gmail.com>
2025-01-06 16:27:45 -0300

    Added pay function

--- views/view.py
+++ views/view.py
@@ -20,13 +20,30 @@ class SubscriptionManagement():
             results = session.exec(statement).all() ##o exec é utilizado para executar querys, e estamos falando para executar statement, que vai selecionar a tabela inteira
             return results
 
+    def _has_pay(self, results):  ##colocamos _ quando a função não será acessado por fora
+        for result in results:
+            if result.date.month() == date.today().month:
+                return True
+        return False
+    
     def pay(self, subscription: Subscription):
         with Session(self.engine) as session:
-            statement = select(Payments)
+            statement = select(Payments).join(Subscription).where(Subscription.empresa == "subscription.empresa") ##To verificando se a empresa que eu quero pagar assinatura, ja está paga ou não
+            results = session.exec(statement).all()           
+            
+            if self._has_pay(results): ##Estamos utilizando a função has pay, para verificar se ja esta pago
+                question = input("Essa conta já foi paga esse mês,deseja pagar novamente? Y ou N: ")
+
+                if not question.upper() == "Y":
+                    return ##Mesma coisa que return None, então paramos a função aqui
+            
+            pay = Payments(subscription_id=subscription.id, date=date.today())
+            session.add(pay)
+            session.commit()
+
+    
 
 
 sm = SubscriptionManagement(engine)
 #subscription = Subscription(empresa='netflix', site='netflix.com.br', data_assinatura=date.today(), valor= '150')
-print(sm.list_all())
-
 
